674745370cdbeea69d3f74724faa2a24db1a57ee
wgl <wgl373711794@163.com>
2020-03-04 17:39:35 +0800

    wgl,定制主题尝试

--- src/views/product/prd_layer.vue
+++ src/views/product/prd_layer.vue
@@ -1,21 +1,27 @@
 <template>
-    <div>
-			<p class="wen">默认按钮</p>
-      <van-button>默认按钮</van-button>
-    </div>
+  <div>
+    <van-tabs v-model="active">
+			<van-tab title="标签 1">内容 1</van-tab>
+			<van-tab title="标签 2">内容 2</van-tab>
+		</van-tabs>
+  </div>
 </template>
 
 <script>
-import { Button } from 'vant';
-	export default {
-		components: { 
-			[Button.name]: Button
-		}
-	}
+import { Tab, Tabs } from 'vant';
+export default {
+  components: {
+		[Tab.name]: Tab,
+		[Tabs.name]: Tabs
+	},
+	data() {
+    return {
+      active: 2
+    };
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-	.wen {
-		font-size: 0.14rem;
-	}
+
 </style>
--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,28 @@ const path = require('path');
 module.exports = {
     publicPath: "./",
     lintOnSave: false,
+    css: {
+        loaderOptions: {
+            less:{
+                // http://lesscss.org/usage/#less-options-strict-units `Global Variables`
+                // `primary` is global variables fields name
+                globalVars: {
+                    // 或者可以通过 less 文件覆盖（文件路径为绝对路径）
+                    'hack': `true; @import "./src/assets/css/theme.less";`
+                },
+                modifyVars: {
+                    // 或者可以通过 less 文件覆盖（文件路径为绝对路径）
+                    'hack': `true; @import "./src/assets/css/theme.less";`
+                }
+            }
+        }
+    },
+    pluginOptions: {
+        'style-resources-loader': {
+            preProcessor: 'less',
+            patterns: [path.resolve(__dirname, './src/assets/css/theme.less')]
+        }
+    },
     configureWebpack: (config) => {
         config.entry.app = ['babel-polyfill', './src/main.js']
         // if (process.env.NODE_ENV === 'prd') {

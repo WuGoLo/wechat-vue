ca75afb9ea31b9085001d5d14d96516fbc190ee5
xiaoting940 <anjing940@126.com>
2020-03-04 19:13:36 +0800

    支付

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "@/views/Home.vue";
 import Product from "@/views/product/prd_layer.vue";
-
+import Payment from "@/views/payment/pay_order.vue";
 Vue.use(VueRouter);
 
 const routes = [
@@ -21,6 +21,19 @@ const routes = [
         path: "details",
         name: "prdDetails",
         component: () => import("@/views/product/prd_details.vue")
+      }
+    ],
+  },
+  {
+    path: "/payment",
+    name: "payment",
+    redirect: '/payment/order',
+    component: Payment,
+    children: [
+      {
+        path: "order",
+        name: "payOrder",
+        component: () => import("@/views/payment/pay_order.vue")
       }
     ],
   }
---
+++ src/views/payment/pay_order.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="home">
+    <div class="header">
+      <van-icon name="arrow-left" />
+    </div>
+    <div class="address">
+    </div>
+    <div class="address">
+
+    </div>
+    <div class="address">
+
+    </div>
+  </div>
+</template>
+
+<script>
+import { Icon, } from 'vant';
+	export default {
+        components: { 
+            [Icon.name]: Icon,
+        }
+	}
+</script>
+<style lang="scss">
+$bg-color: #ffffff;
+  .header{
+    width: 100%;
+    height: 0.45rem;
+    line-height: 0.45rem;
+  }
+  .address{
+    width: 100%;
+    height: 1.5rem;
+    background: #ff6c01;
+    margin-bottom: 0.1rem; 
+  }
+
+</style>

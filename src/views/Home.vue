bba8fcbdf1013ad0c1964cf367e312f73207232a
wgl <wgl373711794@163.com>
2020-03-05 15:45:19 +0800

    详情样式提交

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,40 @@
 <template>
   <div class="home">
     <div class="header">
-      <van-tabs bind:click="onClick">
+      <van-tabs bind:click="onClick" sticky>
         <van-tab title="商品">
           <prd-buy></prd-buy>
         </van-tab>
         <van-tab title="详情">
-
+          <prd-details></prd-details>
         </van-tab>
       </van-tabs>
+    </div>
+    <div class="purchase-box">
+      <van-stepper v-model="value" input-width="0.8rem" button-size="0.46rem"/>
+      <van-button type="primary" size="large" class="btn">立即购买</van-button>
     </div>
   </div>
 </template>
 
 <script>
-  import { Tabs, Tab } from 'vant';
+  import { Tabs, Tab, Stepper, Button } from 'vant';
   import ProBuy from '@/views/product/prd_buy.vue'
+  import ProDetails from '@/views/product/prd_details.vue'
 	export default {
 		components: { 
       [Tabs.name]: Tabs,
       [Tab.name]: Tab,
-      [ProBuy.name]: ProBuy
-		}
+      [ProBuy.name]: ProBuy,
+      [ProDetails.name]: ProDetails,
+      [Stepper.name]: Stepper,
+      [Button.name]: Button,
+    },
+    data() {
+      return {
+        value: 1,
+      }
+    }
 	}
 </script>
 <style lang="scss">
@@ -32,6 +45,10 @@ $bg-color: #ffffff;
   }
   .van-tabs--line .van-tabs__wrap {
     height: 0.45rem;
+  }
+  .van-sticky {
+    background-color: $gray-2;
+    padding-bottom: 0.1rem;
   }
   .van-tabs__line {
     background-color: #04b164;
@@ -47,8 +64,32 @@ $bg-color: #ffffff;
     color: #333333;
   }
   .van-tabs__content{
-    margin-top: 0.1rem;
-    background: $bg-color;
+    background: $gray-2;
   }
-
+  .purchase-box {
+    width: 100%;
+    background-color: #fff;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0.06rem 0.1rem;
+    box-sizing: border-box;
+    position: fixed;
+    bottom: 0;
+    .van-stepper {
+      flex: 1;
+      width: 50%;
+    }
+    .van-button {
+      width: 50%;
+      .btn {
+        border-radius: 0;
+        height: 0.46rem;
+      }
+    }
+    .van-button--large {
+      height: 0.46rem;
+      line-height: 0.44rem;
+    }
+  }
 </style>
---
+++ src/views/product/v_anchor.vue
@@ -0,0 +1,73 @@
+<template>
+ 	<div class="anchor-box">
+		<div class="anchor-nav">
+			<a 
+				href="javascript:void(0)" 
+				rel="external nofollow" 
+				@click="goAnchor('#anchor-'+item.index, item.index)"
+				v-for="item in list" 
+				:key="item.index"
+				:class="item.index == activeClass ? 'active' : ''"
+			>
+				{{item.name}}
+			</a>
+		</div>
+		<div class="anchor-content">
+			<slot></slot>
+		</div>
+	</div>
+</template>
+<script>
+export default {
+	name: 'v-anchor',
+	props: {
+		list: {
+			type: Array,
+			default: () => {
+				return []
+			}
+		}
+	},
+	data(){
+		return {
+			activeClass: 1,
+		}
+	},
+	methods: {
+		goAnchor(selector, index) {
+			this.activeClass = index;
+			var anchor = this.$el.querySelector(selector)
+			var content = this.$el.querySelector('.anchor-content')
+			document.documentElement.scrollTop = anchor.offsetTop - content.offsetTop
+		}
+	}
+}
+</script>
+<style lang="scss" scoped>
+.anchor-box {
+	overflow: hidden;
+}
+	.anchor-nav {
+		width: 100%;
+		height: 0.4rem;
+		line-height: 0.4rem;
+		display: flex;
+		justify-content: space-between;
+		background-color: #fff;
+		margin-bottom: 0.1rem;
+		position: fixed;
+		a {
+			color: $gray-8;
+			font-size: 0.14rem;
+			text-align: center;
+			display: inline-block;
+			flex: 1;
+			&.active {
+				color: $green;
+			}
+		}
+	}
+	.anchor-content {
+		margin-top: 0.5rem;
+	}
+</style>
